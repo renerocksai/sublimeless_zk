74e07ed12aaa477f58ef81a56d2a99cedb3669c4
Rene Schallner <rene@rocks.ai>
2018-04-13 16:19:14 +0200

    initial commit

---
+++ src/imagescintilla.py
@@ -0,0 +1,155 @@
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.Qsci import *
+
+
+class ImageScintilla(QsciScintilla):
+    """
+    Specialized QScintilla editor that allows insertion and deletion
+    of images into the editor itself.
+    """
+
+    class Image:
+        """
+        Class for holding an image's information
+        """
+
+        def __init__(self, image, position, size=None):
+            if isinstance(image, str) == False:
+                raise Exception("Enter path to image as a string.")
+            elif isinstance(position, tuple) == False or len(position) != 2:
+                raise Exception("Image position should be of type tuple(int, int)!")
+            elif size != None and (isinstance(position, tuple) == False \
+                                           or len(position) != 2):
+                raise Exception("Image size has to be of type tuple(int, int)!")
+            # Assign the properties
+            self.image = QImage(image)
+            if size != None:
+                self.image = self.image.scaled(*size)
+            self.position = position
+            self.size = size
+    ''''''
+
+    maximum_image_count = 100
+    image_list = None
+    calculation_font = None
+    line_list = None
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.image_list = {}
+        self.line_list = []
+        # Connect the special SCN_MODIFIED signal, that gives details of
+        # changes in the editor's text.
+        self.SCN_MODIFIED.connect(self.text_changed)
+    ''''''
+
+    def set_calculation_font(self, font):
+        """
+        Set the font for calculating the font metrics like font width and height
+        Parameters:
+            font -> QFont, a QFont that will be used for the calculations
+        """
+        if isinstance(font, QFont) == False:
+            raise Exception("The calculation font has to be a QFont!")
+        self.calculation_font = font
+    ''''''
+
+    def add_image(self, image, position, size):
+        """
+        Add the image to the image list.
+        Parameters:
+            image -> str, path to image
+            position -> tuple(int, int), the COLUMN and LINE offset for the image position
+            size -> tuple(int, int), the height and width of the displayed image.
+                    If None, the original image size will be used.
+        Return value:
+            index -> int, index of the added image
+        """
+        image = self.Image(image, position, size)
+        for i in range(self.maximum_image_count):
+            if not (i in self.image_list.keys()):
+                self.image_list[i] = image
+                return i
+        else:
+            raise Exception("Too many images in the editor, the maximum is '{}'"\
+                            .format(self.maximum_image_count))
+    ''''''
+
+    def delete_image(self, index):
+        """
+        Delete an image from the image list using it's index number
+        Parameters:
+            index -> int, the index of the image
+        """
+        if isinstance(index, int) == False or index > self.maximum_image_count:
+            raise Exception(
+                "Index for deletion should be smaller integer than maximum_image_count")
+        # Delete the image from the image list by
+        # poping the entry out of the dictionary!
+        self.image_list.pop(index, None)
+    ''''''
+
+    def get_font_metrics(self, font):
+        font_metrics = QFontMetrics(font)
+        single_character_width = font_metrics.width("A")  # Works for monospaced fonts!
+        # For other fonts it is a good enough estimate.
+        single_character_height = font_metrics.height()
+        return single_character_width, single_character_height
+    ''''''
+
+    def text_changed(self,
+                     position,
+                     mod_type,
+                     text,
+                     length,
+                     lines_added,
+                     line,
+                     fold_level_now,
+                     fold_level_prev,
+                     token,
+                     additional_lines_added):
+        """
+        This is a function connected to the SCN_MODIFIED signal.
+        It gives great information of what changes happened in the editor.
+        """
+        insert_flag = mod_type & 0b1
+        delete_flag = mod_type & 0b10
+        if insert_flag or delete_flag:
+            change_line, change_column = self.lineIndexFromPosition(position)
+            #            print(change_line, lines_added)
+            if lines_added != 0:
+                # Loop through the lines and adjust the Y(LINE) position offset
+                for key, image in self.image_list.items():
+                    x, y = image.position
+                    if y >= change_line:
+                        image.position = (x, y + lines_added)
+    ''''''
+
+    def paintEvent(self, e):
+        super().paintEvent(e)
+        # Get the painting frame details
+        current_parent_size = self.size()
+        first_visible_line = self.SendScintilla(self.SCI_GETFIRSTVISIBLELINE)
+        column_offset_in_pixels = self.SendScintilla(self.SCI_GETXOFFSET)
+        single_character_width, single_character_height = self.get_font_metrics(
+            self.calculation_font
+        )
+        # Initialize the painter
+        painter = QPainter()
+        painter.begin(self.viewport())
+        # Loop throught the images and paint them
+        for i in self.image_list.keys():
+            # Set the paint offsets
+            image = self.image_list[i].image
+            paint_offset_x = (self.image_list[i].position[0] * single_character_width)\
+                             - column_offset_in_pixels
+            paint_offset_y = (self.image_list[i].position[1] - first_visible_line)\
+                             * single_character_height
+            # Paint the image
+            painter.drawImage(QPoint(paint_offset_x, paint_offset_y), image)
+        # Close the painter
+        painter.end()
+    ''''''
---
+++ src/zklexer.py
@@ -0,0 +1,367 @@
+# -*- coding: utf-8 -*-
+import os
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.Qsci import *
+import re
+import json
+
+class MyLexer(QsciLexerCustom):
+    def __init__(self, parent, theme_file):
+        super(MyLexer, self).__init__(parent)
+        self.style_infos = {}
+        self.style2id = {}
+        self.id2stylename = {}
+        self.font_info = {}
+        self.caret = None
+        self.hightlight = None
+        self.selection = None
+        self.load_theme(theme_file)
+        for styleid, style in enumerate(self.style_infos):
+            self.style2id[style] = styleid
+            self.id2stylename[styleid] = style
+
+        # Default text settings
+        # ----------------------
+        self.setDefaultColor(QColor(self.style_infos['default']['color']))
+        self.setDefaultPaper(QColor(self.style_infos['default']['background']))
+        weight = QFont.Normal
+        italic = False
+        if self.font_info['style'] == 'bold':
+            weight = QFont.Bold
+        elif self.font_info['style'] == 'italic':
+            italic = True
+        elif self.font_info['style'] == 'bolditalic':
+            italic = True
+            weight = QFont.Bold
+        self.default_font =  QFont(self.font_info['face'], self.font_info['size'], weight=weight, italic=italic)
+        self.setDefaultFont(self.default_font)
+
+        default_weight = weight
+        default_italic = italic
+        default_font = self.font_info['face']
+        default_size = self.font_info['size']
+
+        for styleid, style in enumerate(self.style_infos):
+            print('Initializing style', styleid, style, self.style_infos[style])
+            self.style2id[style] = styleid
+            self.id2stylename[styleid] = style
+            weight = default_weight
+            italic = default_italic
+            current_style = self.style_infos[style]
+            if ['style'] == 'bold':
+                weight = QFont.Bold
+            elif current_style['style'] == 'italic':
+                italic = True
+            elif current_style['style'] == 'bolditalic':
+                italic = True
+                weight = QFont.Bold
+            elif current_style['style'] == 'normal':
+                italic = False
+                weight = QFont.Normal
+            self.setColor(QColor(current_style['color']), styleid)
+            self.setPaper(QColor(current_style['background']), styleid)
+            self.setFont(QFont(default_font, default_size, weight=weight, italic=italic), styleid)
+            if styleid > 64:
+                break
+
+    ''''''
+
+    def get_style(self, d, key):
+        ret = d.get(key, {})
+        ret['color'] = ret.get('color', self.style_infos['default']['color'])
+        ret['background'] = ret.get('background', self.style_infos['default']['background'])
+        ret['style'] = ret.get('style', self.style_infos['default']['style'])
+        return ret
+
+    def get_symbol_text(self, d, key):
+        themed = d.get(key, {})
+        for item in 'symbol', 'text':
+            dd = self.get_style(themed, item)
+            themed[item] = dd
+        return themed
+
+    def get_theme_style(self, style):
+        self.style_infos[style] = self.get_style(self.theme_d, style)
+
+    def get_theme_symbol_text(self, style):
+        ret = self.get_symbol_text(self.theme_d, style)
+        self.style_infos[style + '.' + 'symbol'] = ret['symbol']
+        self.style_infos[style + '.' + 'text'] = ret['text']
+
+    def load_theme(self, theme_file):
+        if not os.path.exists(theme_file):
+            return False
+        with open(theme_file, 'rt') as f:
+            theme_d = json.load(f)
+
+        # now fill in the blanks
+        self.theme_d = theme_d
+        background = theme_d.get('background', "#fffdf6e3")
+        foreground = theme_d.get('foreground', "#ff657b83")
+
+        self.highlight = theme_d.get('linehighlight', "#3F3D3812")
+        self.selection = theme_d.get('selection', {})
+        self.selection['background'] = self.selection.get('background', '#ffd33682')
+        self.selection['foreground'] = self.selection.get('foreground', '#fffdf6e3')
+        self.caret = theme_d.get('caret', "#ff6ec3dc")
+
+        font = theme_d.get('font', {})
+        font['face'] = font.get('face', 'Ubuntu Mono')
+        font['size'] = font.get('size', 16)
+        font['style'] = font.get('style', 'normal')
+        self.font_info = font
+        self.style_infos['default'] = {
+            'color': foreground,
+            'background': background,
+            'style': font['style']
+        }
+
+        for style in 'text.italic', 'text.bold', 'text.bolditalic', 'quote', \
+                     'list.ordered', 'list.unordered':
+            self.get_theme_symbol_text(style)
+        for i in range(6):
+            hname = f'h{i+1}'
+            self.get_theme_symbol_text(hname)
+        for style in 'code.block', 'code.inline', 'tag', 'citekey', 'comment', \
+                'footnote':
+            self.get_theme_style(style)
+
+        code_fenced_dict = theme_d.get('code.fenced', {})
+        self.style_infos['code.fenced.language'] = self.get_style(code_fenced_dict, 'language')
+        self.style_infos['code.fenced'] = self.get_style(code_fenced_dict, 'text')
+
+        link_dict = theme_d.get('link', {})
+        self.style_infos['link.title'] = self.get_style(link_dict, 'title')
+        self.style_infos['link.url'] = self.get_style(link_dict, 'url')
+
+        zettel_link_dict = theme_d.get('zettel.link', {})
+        self.style_infos['zettel.link.title'] = self.get_style(zettel_link_dict, 'title')
+        self.style_infos['zettel.link.noteid'] = self.get_style(zettel_link_dict, 'noteid')
+
+        image_dict = theme_d.get('image', {})
+        self.style_infos['image.caption'] = self.get_style(image_dict, 'caption')
+        self.style_infos['image.link'] = self.get_style(image_dict, 'link')
+        self.style_infos['image.attr'] = self.get_style(image_dict, 'attr')
+
+
+    def language(self):
+        return "MardownZettelkasten"
+    ''''''
+
+    def description(self, style):
+        if style in self.id2stylename:
+            return self.id2stylename[style]
+        else:
+            return ''
+    ''''''
+
+    def styleText3(self, start, end):
+        self.startStyling(start)
+        text = self.parent().text()[start:end]
+        self.setStyling(end-start, self.style2id['default'])
+
+    def styleText(self, start, end):
+        self.startStyling(0)
+        text = bytearray(self.parent().text(), "utf-8").decode("utf-8")
+        p = re.compile(r'([\[]?\[)([0-9.]{12,18})([^]]*)(\][\]]?)')
+        regions = []
+        for match in p.finditer(text):
+            regions.append((match.start(), match.end(), match.group()))
+            print(match.group())
+
+        style_regions = []
+        # now translate regions to byte arrays
+        current_pos = 0
+        for region in regions:
+            gap = region[0] - current_pos
+            gap_b = len(bytearray(text[current_pos:region[0]], 'utf-8'))
+            if gap_b > 0:
+                style_regions.append((gap_b, 'default'))
+            match_b = len(bytearray(region[2], 'utf-8'))
+            match = region[1] - region[0]
+            style_regions.append((match_b, 'zettel.link.noteid'))
+            current_pos += gap + match
+        gap = len(text) - current_pos
+        if gap > 0:
+            gap_b = len(bytearray(text[current_pos:], 'utf-8'))
+            style_regions.append((gap_b, 'default'))
+        print(start, end)
+
+        for num_chars, style in style_regions:
+            self.setStyling(num_chars, self.style2id[style])
+
+    def styleText2(self, start, end):
+        # 1. Initialize the styling procedure
+        # ------------------------------------
+        self.startStyling(start)
+
+        # 2. Slice out a part from the text
+        # ----------------------------------
+        text = self.parent().text()[start:end]
+
+        # 3. Tokenize the text
+        # ---------------------
+        p = re.compile(r"-->|<!--|\s+|\w+|\W")
+
+        # 'token_list' is a list of tuples: (token_name, token_len)
+        token_list = [ (token, len(bytearray(token, "utf-8"))) for token in p.findall(text)]
+
+        # 4. Style the text
+        # ------------------
+        # 4.1 Check if multiline comment
+        multiline_comm_flag = False
+        editor = self.parent()
+        if start > 0:
+            previous_style_nr = editor.SendScintilla(editor.SCI_GETSTYLEAT, start - 1)
+            if previous_style_nr == 3:
+                multiline_comm_flag = True
+            ###
+        ###
+        # 4.2 Style the text in a loop
+        for i, token in enumerate(token_list):
+            if multiline_comm_flag:
+                self.setStyling(token[1], 3)
+                if token[0] == "-->":
+                    multiline_comm_flag = False
+                ###
+            ###
+            else:
+                if token[0] in ["for", "while", "return", "int", "include"]:
+                    # Red style
+                    self.setStyling(token[1], 1)
+
+                elif token[0] in ["(", ")", "{", "}", "[", "]", "#"]:
+                    # Blue style
+                    self.setStyling(token[1], 2)
+
+                elif token[0] == "<!--":
+                    multiline_comm_flag = True
+                    self.setStyling(token[1], 3)
+
+                else:
+                    # Default style
+                    self.setStyling(token[1], self.style2id['default'])
+                ###
+            ###
+        ###
+
+    ''''''
+
+''' end Class '''
+
+
+class CustomMainWindow(QMainWindow):
+    def __init__(self):
+        super(CustomMainWindow, self).__init__()
+
+        # -------------------------------- #
+        #           Window setup           #
+        # -------------------------------- #
+
+        # 1. Define the geometry of the main window
+        # ------------------------------------------
+        self.setGeometry(300, 300, 800, 400)
+        self.setWindowTitle("QScintilla Test")
+
+        # 2. Create frame and layout
+        # ---------------------------
+        self._frm = QFrame(self)
+        self._frm.setStyleSheet("QWidget { background-color: #ffeaeaea }")
+        self._lyt = QVBoxLayout()
+        self._frm.setLayout(self._lyt)
+        self.setCentralWidget(self._frm)
+        self._myFont = QFont()
+        self._myFont.setPointSize(14)
+
+        # 3. Place a button
+        # ------------------
+        self._btn = QPushButton("Qsci")
+        self._btn.setFixedWidth(50)
+        self._btn.setFixedHeight(50)
+        self._btn.clicked.connect(self._btn_action)
+        self._btn.setFont(self._myFont)
+        self._lyt.addWidget(self._btn)
+
+        # -------------------------------- #
+        #     QScintilla editor setup      #
+        # -------------------------------- #
+
+        # ! Make instance of QSciScintilla class!
+        # ----------------------------------------
+        self._editor = QsciScintilla()
+        self._editor.setUtf8(True)             # Set encoding to UTF-8
+        with open('zettelkasten/201803100758 P.md', mode='r', encoding='utf-8', errors='ignore') as f:
+            txt = f.read()
+        self._editor.setText(txt)     # 'myCodeSample' is a string containing some C-code
+        self._editor.setLexer(None)            # We install lexer later
+        #self._editor.setFont(self._myFont)    # Gets overridden by lexer later on
+
+        # 1. Text wrapping
+        # -----------------
+        self._editor.setWrapMode(QsciScintilla.WrapWord)
+        self._editor.setWrapVisualFlags(QsciScintilla.WrapFlagByText)
+        self._editor.setWrapIndentMode(QsciScintilla.WrapIndentIndented)
+
+        # 2. End-of-line mode
+        # --------------------
+        self._editor.setEolMode(QsciScintilla.EolWindows)
+        self._editor.setEolVisibility(False)
+
+        # 3. Indentation
+        # ---------------
+        self._editor.setIndentationsUseTabs(False)
+        self._editor.setTabWidth(4)
+        self._editor.setIndentationGuides(True)
+        self._editor.setTabIndents(True)
+        self._editor.setAutoIndent(True)
+
+
+        # 5. Margins
+        # -----------
+        # Margin 0 = Line nr margin
+        #self._editor.setMarginType(0, QsciScintilla.NumberMargin)
+        self._editor.setMarginType(0, QsciScintilla.SymbolMargin)
+        self._editor.setMarginWidth(0, "0000")
+        self._editor.setMarginWidth(1, "0000")
+        self._editor.setMarginsForegroundColor(QColor("#ff888888"))
+
+        # -------------------------------- #
+        #          Install lexer           #
+        # -------------------------------- #
+        if True:
+            self._lexer = MyLexer(self._editor, 'theme.json')
+            self._editor.setLexer(self._lexer)
+
+        # 4. Caret
+        # ---------
+        self._editor.setCaretForegroundColor(QColor(self._lexer.caret))
+        self._editor.setCaretLineVisible(True)
+        self._editor.setCaretLineBackgroundColor(QColor(self._lexer.highlight))
+        self._editor.setCaretWidth(8)
+        #self._editor.setMarginsBackgroundColor(QColor("#ff404040"))
+        self._editor.setFont(self._lexer.default_font)
+
+        # ! Add editor to layout !
+        # -------------------------
+        self._lyt.addWidget(self._editor)
+        self.show()
+
+    ''''''
+
+    def _btn_action(self):
+        print("Hello World!")
+    ''''''
+
+''' End Class '''
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    QApplication.setStyle(QStyleFactory.create('Fusion'))
+    myGUI = CustomMainWindow()
+
+    sys.exit(app.exec_())
+
+''''''
