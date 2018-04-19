b3b6e2ac26ad72c75380b9b833ad9f5c513c5785
Rene Schallner <rene@rocks.ai>
2018-04-19 02:08:05 +0200

    all actions

--- src/sublimeless_zk.py
+++ src/sublimeless_zk.py
@@ -18,34 +18,68 @@ class Sublimeless_Zk(QObject):
 
 
     def init_actions(self):
-        """
-        { "caption": "ZK: New Zettel Note", "command": "zk_new_zettel" },
-        { "caption": "ZK: Insert Citation", "command": "zk_insert_citation" },
-        { "caption": "ZK: Pick tag from list", "command": "zk_tag_selector" },
-        { "caption": "ZK: Find all tags in archive", "command": "zk_show_all_tags" },
-        { "caption": "ZK: Expand Overview Note", "command": "zk_expand_overview_note" },
-        { "caption": "ZK: Refresh Expanded Note", "command": "zk_refresh_expanded_note" },
-        { "caption": "ZK: Expand Link inline", "command": "zk_expand_link" },
-        { "caption": "ZK: Search for tag combination", "command": "zk_multi_tag_search" },
-        { "caption": "ZK: Auto-Bib", "command": "zk_auto_bib" },
-        { "caption": "ZK: Show Images", "command": "zk_show_images" },
-        { "caption": "ZK: Hide Images", "command": "zk_hide_images" },
-        { "caption": "ZK: Auto-TOC", "command": "zk_toc" },
-        { "caption": "ZK: Number Headings", "command": "zk_renumber_headings" },
-        { "caption": "ZK: Remove Heading Numbers", "command": "zk_denumber_headings" },
-        { "caption": "ZK: Select Panes for opening notes/results", "command": "zk_select_panes" },
-        { "caption": "ZK: Insert Link", "command": "zk_get_wiki_link" },
-        { "caption": "ZK: Show all Notes", "command": "zk_show_all_notes" },
-        { "caption": "ZK: Enter Zettelkasten Mode", "command": "zk_enter_zk_mode" },
-        """
         self.newAction = QAction("New Zettel Note", self)
         self.newAction.setShortcuts(["Ctrl+N", "Shift+Return", "Shift+Enter"])
-        self.newAction.triggered.connect(self.zk_new_zettel)
 
         self.insertLinkAction = QAction('Insert Link to Note', self)
         self.insertLinkAction.setShortcut('[,[')
-        self.insertLinkAction.triggered.connect(self.insert_link)
-        self.gui.editor.text_shortcut_handler.shortcut_insert_link.connect(self.insert_link)
+
+        self.showReferencingNotesAction = QAction('Show referencing notes', self)
+        self.showReferencingNotesAction.setShortcut('Alt+Enter')
+
+        self.insertTagAction = QAction('Insert Tag', self)
+        self.insertTagAction.setShortcut('#, ?')
+
+        self.showTagsAction = QAction('Show all Tags', self)
+        self.showTagsAction.setShortcut('#,!')
+
+        self.expandLinkAction = QAction('Expand link', self)
+        self.expandLinkAction.setShortcut('Ctrl+.')
+        if sys.platform == 'darwin':
+            self.expandLinkAction.setShortcut('Meta+.')
+
+        self.insertCitationAction = QAction('Insert Citation', self)
+        self.insertCitationAction.setShortcuts(['[,@', '[,#'])
+
+        self.expandOverviewNoteAction = QAction('Expand Overview Note', self)
+        self.expandOverviewNoteAction.setShortcut('Ctrl+E')
+
+        self.refreshExpandedNoteAction = QAction('Refresh expanded Note', self)
+        self.refreshExpandedNoteAction.setShortcut('Ctrl+R')
+
+        self.advancedTagSearchAction = QAction('Search for tag combination', self)
+        self.advancedTagSearchAction.setShortcut('Ctrl+T')
+
+        self.autoBibAction = QAction('Insert Bibliography', self)
+        self.autoBibAction.setShortcut('Ctrl+B')
+
+        self.showImagesAction = QAction('Show Images', self)
+        self.showImagesAction.setShortcut('Shift+Ctrl+I')
+
+        self.hideImagesAction = QAction('Hide Images', self)
+        self.hideImagesAction.setShortcut('Shift+Ctrl+H')
+
+        self.autoTocAction = QAction('Insert Table of Contents', self)
+        self.autoTocAction.setShortcut('Shift+Ctrl+T')
+
+        self.numberHeadingsAction = QAction('Insert Section Numbers', self)
+        self.numberHeadingsAction.setShortcut('Ctrl+Shift+I')
+
+        self.denumberHeadingsAction = QAction('Remove Section Numbers', self)
+        self.denumberHeadingsAction.setShortcut('Ctrl+Shift+R')
+
+        self.showAllNotesAction = QAction('Show all Notes', self)
+        self.showAllNotesAction.setShortcut('[,!')
+
+        """
+        { "caption": "ZK: Auto-TOC", "command": "zk_toc" },
+        { "caption": "ZK: Number Headings", "command": "zk_renumber_headings" },
+        { "caption": "ZK: Remove Heading Numbers", "command": "zk_denumber_headings" },
+        { "caption": "ZK: Show all Notes", "command": "zk_show_all_notes" },
+        """
+
+
+
 
 
         ## Editor shortcut overrides
@@ -89,6 +123,7 @@ class Sublimeless_Zk(QObject):
         file = menubar.addMenu("File")
         edit = menubar.addMenu("Edit")
         view = menubar.addMenu("View")
+        tools = menubar.addMenu('Tools')
         about = menubar.addMenu("About")
 
         file.addAction(self.newAction)
@@ -101,6 +136,15 @@ class Sublimeless_Zk(QObject):
         # file.addAction(self.dirAct)
 
         edit.addAction(self.insertLinkAction)
+        edit.addAction(self.showReferencingNotesAction)
+        edit.addAction(self.insertTagAction)
+        edit.addAction(self.expandLinkAction)
+        edit.addAction(self.insertCitationAction)
+        edit.addAction(self.autoBibAction)
+        edit.addAction(self.autoTocAction)
+        edit.addAction(self.numberHeadingsAction)
+        edit.addAction(self.denumberHeadingsAction)
+
         # edit.addAction(self.undoAction)
         # edit.addAction(self.redoAction)
         # edit.addSeparator()
@@ -109,19 +153,53 @@ class Sublimeless_Zk(QObject):
         # edit.addAction(self.pasteAction)
         # edit.addAction(self.FRAct)
 
-        # view.addAction(self.treeAct)
+        view.addAction(self.showAllNotesAction)
+        view.addAction(self.showTagsAction)
+        view.addAction(self.showImagesAction)
+        view.addAction(self.hideImagesAction)
+
+        tools.addAction(self.expandOverviewNoteAction)
+        tools.addAction(self.refreshExpandedNoteAction)
+        tools.addAction(self.advancedTagSearchAction)
 
         # about.addAction(self.aboutAction)
 
     def connect_signals(self):
+        # lexer actions
         self.gui.lexer.tag_clicked.connect(self.clicked_tag)
         self.gui.lexer.note_id_clicked.connect(self.clicked_noteid)
         self.gui.search_results_editor.lexer().tag_clicked.connect(self.clicked_tag)
         self.gui.search_results_editor.lexer().note_id_clicked.connect(self.clicked_tag)
         self.gui.saved_searches_editor.lexer().tag_clicked.connect(self.clicked_tag)
         self.gui.saved_searches_editor.lexer().note_id_clicked.connect(self.clicked_tag)
+
+        # tab actions
         self.gui.qtabs.tabCloseRequested.connect(self.gui.qtabs.removeTab)
         self.gui.qtabs.tabCloseRequested.connect(self.lessTabs)
+
+        # normal actions
+        self.newAction.triggered.connect(self.zk_new_zettel)
+        self.insertLinkAction.triggered.connect(self.insert_link)
+        self.showReferencingNotesAction.triggered.connect(self.show_referencing_notes)
+        self.insertTagAction.triggered.connect(self.insert_tag)
+        self.showTagsAction.triggered.connect(self.show_all_tags)
+        self.expandLinkAction.triggered.connect(self.expand_link)
+        self.insertCitationAction.triggered.connect(self.insert_citation)
+        self.expandOverviewNoteAction.triggered.connect(self.expand_overview_note)
+        self.refreshExpandedNoteAction.triggered.connect(self.refresh_expanded_note)
+        self.advancedTagSearchAction.triggered.connect(self.advanced_tag_search)
+        self.autoBibAction.triggered.connect(self.auto_bib)
+        self.showImagesAction.triggered.connect(self.show_images)
+        self.hideImagesAction.triggered.connect(self.hide_images)
+        self.autoTocAction.triggered.connect(self.auto_toc)
+        self.numberHeadingsAction.triggered.connect(self.number_headings)
+        self.denumberHeadingsAction.triggered.connect(self.denumber_headings)
+        self.showAllNotesAction.triggered.connect(self.show_all_notes)
+
+        # text shortcut actions
+        self.gui.editor.text_shortcut_handler.shortcut_insert_link.connect(self.insert_link)
+
+
 
     def run(self):
         self.app = QApplication(sys.argv)
@@ -166,6 +244,54 @@ class Sublimeless_Zk(QObject):
 
     def insert_link(self):
         print('Insert Link')
+
+    def show_referencing_notes(self):
+        print('Show referencing note')
+
+    def insert_tag(self):
+        print('insert tag')
+
+    def show_all_tags(self):
+        print('show all tags')
+
+    def expand_link(self):
+        print('expand link')
+
+    def insert_citation(self):
+        print('insert citation')
+
+    def show_all_notes(self):
+        print('show all notes')
+
+    def expand_overview_note(self):
+        print('expand overview note')
+
+    def refresh_expanded_note(self):
+        pass
+
+    def advanced_tag_search(self):
+        pass
+
+    def auto_bib(self):
+        pass
+
+    def show_images(self):
+        pass
+
+    def hide_images(self):
+        pass
+
+    def auto_toc(self):
+        pass
+
+    def number_headings(self):
+        pass
+
+    def denumber_headings(self):
+        pass
+
+    def show_all_notes(self):
+        pass
 
 if __name__ == '__main__':
     Sublimeless_Zk().run()
